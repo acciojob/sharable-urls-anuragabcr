b2d8de7385e2cf93bfb70da6e2975f0a76346c11
Anurag Kumar <47923173+anuragabcr@users.noreply.github.com>
2024-05-18 10:19:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // your code here
+function onSubmit(event) {
+	event.preventDefault()
+	let name = document.getElementById("name").value
+	let year = document.getElementById("year").value
+	let url = document.getElementById("url")
+	url.innerHTML += `?name=${name}&year=${year}`
+}
